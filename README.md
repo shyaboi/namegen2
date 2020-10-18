a269913f141aede1710669fc1394651ddeb91fe2
Shyaboi <ian.shane.sears@gmail.com>
2020-10-18 00:52:45 -0700

    added top and bottom dummy navs and a button, and added roboto font

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# NameGen 2: All the words
 
+This is my refiinement of my original 2 array name generator that I had made with only 1 HTML/JS file.
+//
+This version uses React for the front end UI, and a node backend server.
+//
+For the purpose of speed, I have condensed the a 400+ word dictonary into a simple array to grab a set amount of random words from an api endpoint to display on anything.
+//
+For my purposes, the word API and the nameGenerator will be seperated a bit. The React app will simply fetch data from the API endpoints, and use those.
+//
+When the react app has reached the end of the array stored in the 
--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import './App.css';
-
-import axios from 'axios';
+import React from "react";
+import axios from "axios"
+import "./App.css";
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import 'fontsource-roboto';
+import BottomNav from "./components/BottomNav/BottomNav"
+import TopNav from "./components/TopNav/TopNav"
 
 export default class App extends React.Component {
   state = {
-    words: []
-  }
+    topWord: "TopWord",
+    words: [],
+  };
 
   componentDidMount() {
-    axios.get(`http://localhost:5000/10`)
-      .then(res => {
-        const words = res.data;
-        this.setState({ words });
-        console.log(this.state.words)
-      })
+    axios.get(`http://localhost:5000/10`).then((res) => {
+      let [terpWord, ...words] = res.data;
+      console.log(terpWord);
+      // const words = res.data;
+      this.setState({ words });
+      console.log(this.state.words);
+      this.setState({ topWord:terpWord });
+    });
   }
 
   render() {
     return (
-    <div className="App">
-
-      <ul>
-        { this.state.words.map(person => <li>{person}</li>)}
-      </ul>
-      </div>
-
-    )
+    <Container >
+      <TopNav />
+      <Box display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
+      <Typography variant="h1" component="h2" gutterBottom key={this.state.topWord}>{this.state.topWord}</Typography>
+</Box>
+<Box display="flex" flexDirection="row-reverse" p={1} m={1} bgcolor="background.paper">
+      <Button variant="contained" color="primary">
+  Primary
+</Button>
+</Box>
+<Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
+    <BottomNav />
+    </Box>
+    </Container>
+     
+    );
   }
 }
