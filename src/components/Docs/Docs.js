1195736c4a88cdbaac2eb23389effb63515437fa
Shyaboi <ian.shane.sears@gmail.com>
2020-10-22 00:45:25 -0700

    started docs page, linked, menu button wonky, but working

---
+++ src/components/Docs/Docs.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import CodeBlock from '@tenon-io/tenon-codeblock';
+import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
+
+export default class Docs extends React.Component {
+render (){
+    return (
+    <Container>
+          <Typography variant="h1" component="h2" gutterBottom>
+        API Documentation
+      </Typography>
+    <CodeBlock codeString={'dfggdf'}/>
+    </Container>
+    )
+}
+}
--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -6,15 +6,24 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import TopWord from "../TopWord/TopWord"
 import MultiWord from "../MultiWord/MultiWord"
-import { MenuItem } from '@material-ui/core';
+import Docs from "../Docs/Docs"
+
+
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    position: 'relative',
+
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -22,19 +31,52 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  dropdown: {
+    position: 'absolute',
+    top: 28,
+    right: 0,
+    left: 0,
+    zIndex: 1,
+    border: '1px solid',
+    padding: theme.spacing(1),
+    backgroundColor: theme.palette.background.paper,
+  },
 }));
 
 export default function TopNav() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
   const classes = useStyles();
 
   return (
     <Router>
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <Toolbar aria-controls="simple-menu" aria-haspopup="true">
+  
+          <ClickAwayListener onClickAway={handleClickAway}>
+          <div className={classes.root}>
+
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleClick}>
             <MenuIcon />
           </IconButton>
+        {open ? (
+          <div className={classes.dropdown}>
+            Click me, I will stay visible until you click outside.
+          </div>
+        ) : null}
+              </div>
+
+    </ClickAwayListener>
+
             <Typography variant="h6" className={classes.title}>
           <Button color="inherit"><MenuItem component={Link} to="/TopWord">Top Word</MenuItem>  </Button>
           </Typography>
@@ -45,10 +87,10 @@ export default function TopNav() {
           
           
            <Typography variant="h6" className={classes.title}>
-          <Button color="inherit">API Documentation</Button>
+          <Button color="inherit"> <MenuItem component={Link} to="/Docs" >API Documentation</MenuItem></Button>
           </Typography>
           <Button color="inherit">Anoter Thing</Button>
-        </Toolbar>
+</Toolbar>
       </AppBar>
 
 
@@ -62,6 +104,9 @@ export default function TopNav() {
           </Route>
           <Route exact path="/MultiWord" component={MultiWord}>
             <MultiWord />
+          </Route>
+          <Route exact path="/Docs" component={Docs}>
+            <Docs />
           </Route>
         </Switch>
     </div>
